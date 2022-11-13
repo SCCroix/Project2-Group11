018b46cd49506d7753d0570a564bd78d8482d77c
Linh Bui <linh@Linhs-iMac.local>
2022-11-13 15:24:29 -0800

    Add changing Img Src for profile photo

--- assets/profileScript.js
+++ assets/profileScript.js
@@ -1,11 +1,15 @@
+// Profile variable
 let editProfileImgSection = document.querySelector("#editImgDiv")
 let editProfileImgButton = document.querySelector("#editProfileImg")
 let uploadProfileImgButton = document.querySelector("#uploadSrclinkProfileImg")
 let cancelProfileImgButton = document.querySelector("#hideEditImgDiv")
+let profilePhoto = document.querySelector("#profilePhoto")
+// Bio Variab;e
 let editProfileBioSection = document.querySelector("#editBioDiv")
 let editProfileBioButton = document.querySelector("#editBioButton")
 let updateBioButton = document.querySelector("#updateBioContentButton")
 let cancelProfileBioButton = document.querySelector("#hideEditBioDiv")
+let bioContent = document.querySelector("#bioContent")
 // Profile Img
 editProfileImgSection.style.display = "none";
 editProfileImgButton.addEventListener("click", function(){
@@ -16,10 +20,12 @@ cancelProfileImgButton.addEventListener("click",function(){
 })
 uploadProfileImgButton.addEventListener("click", function(){
     let confirmChangedProfileImg = confirm("CONFIRM CHANGED: Do you want to change into your new profile image?")
-    // if (confirmChangedProfileImg) { // replace the new input to the src from  ID#profilePhto 
-
-    // }
-    // else // do not replace 
+    if (confirmChangedProfileImg) { // replace the new input to the src from  ID#profilePhto 
+        var x = $("#inputSrcProfileImg:text").val(); // access JQuery library to extract and store the input  of the input element speific to Profile Img src ID 
+        profilePhoto.src = x
+        console.log(x)
+        editProfileImgSection.style.display = "none";
+    } 
 })
 // Bio Content 
 editProfileBioSection.style.display = "none"; 
