d372f88c8c073960b8ac7b5b8154c7947e111f65
Linh Bui <linh@Linhs-iMac.local>
2022-11-13 14:45:48 -0800

    add confirm function while update profile img and bio

--- assets/profileScript.js
+++ assets/profileScript.js
@@ -14,6 +14,13 @@ editProfileImgButton.addEventListener("click", function(){
 cancelProfileImgButton.addEventListener("click",function(){
     editProfileImgSection.style.display = "none";
 })
+uploadProfileImgButton.addEventListener("click", function(){
+    let confirmChangedProfileImg = confirm("CONFIRM CHANGED: Do you want to change into your new profile image?")
+    // if (confirmChangedProfileImg) { // replace the new input to the src from  ID#profilePhto 
+
+    // }
+    // else // do not replace 
+})
 // Bio Content 
 editProfileBioSection.style.display = "none"; 
 editProfileBioButton.addEventListener("click",function(){
@@ -21,6 +28,9 @@ editProfileBioButton.addEventListener("click",function(){
 })
 cancelProfileBioButton.addEventListener("click", function(){
     editProfileBioSection.style.display = "none"
+})
+updateBioButton.addEventListener("click", function(){
+    let confirmEditBiocontent = confirm("CONFIRM EDITED: Do you want to updated new bio content?")
 })
 
 
